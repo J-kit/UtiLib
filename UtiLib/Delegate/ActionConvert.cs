f5814c957c3943bb16cad7244f65190ae411a170
Jonas <admin@j-kit.de>
2018-01-18 00:16:58 +0100

    Imported DelegateConvert to UtiLib.Delegate

---
+++ UtiLib/Delegate/ActionConvert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UtiLib.Delegate
+{
+    public class ActionConvert
+    {
+        /// <summary>
+        /// Converts any generic Action to the given return type
+        /// </summary>
+        /// <param name="sourceDelegate">Should be Action<TX,...></param>
+        /// <returns></returns>
+        public static Action<object[]> Convert(System.Delegate sourceDelegate)
+        {
+            if (sourceDelegate.Method.ReturnType != typeof(void))
+                throw new Exception("Invalid source delegate Type");
+
+            return (Action<object[]>)DelegateConverter.Convert(sourceDelegate);
+        }
+    }
+}
---
+++ UtiLib/Delegate/FuncConvert.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UtiLib.Delegate
+{
+    public class FuncConvert
+    {
+        /// <summary>
+        /// Converts any generic Func to the given return type
+        /// </summary>
+        /// <param name="sourceDelegate">Should be Func<TX,...></param>
+        /// <returns></returns>
+        public static Func<object[], object> Convert(System.Delegate sourceDelegate)
+        {
+            if (sourceDelegate.Method.ReturnType == typeof(void))
+                throw new Exception("Invalid source delegate Type");
+
+            return (Func<object[], object>)DelegateConverter.Convert(sourceDelegate);
+        }
+
+        /// <summary>
+        /// Converts any generic Func to the given return type
+        /// </summary>
+        /// <typeparam name="TReturn">The required return type</typeparam>
+        /// <param name="sourceDelegate">Should be Func<TX,...></param>
+        /// <returns></returns>
+        public static Func<object[], TReturn> Convert<TReturn>(System.Delegate sourceDelegate)
+        {
+            if (sourceDelegate.Method.ReturnType == typeof(void))
+                throw new Exception("Invalid source delegate Type");
+
+            return (Func<object[], TReturn>)DelegateConverter.Convert(sourceDelegate, typeof(TReturn));
+        }
+    }
+}
